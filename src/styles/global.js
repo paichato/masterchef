7b4d77594a7ee15afee55f824b270cf62cf4ea9c
Marlon Strong <tsenane14@gmail.com>
2021-02-06 20:43:54 +0200

    global styles done

--- src/styles/global.js
+++ src/styles/global.js
@@ -6,10 +6,31 @@ export default createGlobalStyle`
     box-sizing:border-box;
     padding:0;
     margin:0;
+    color:var(--secondary);
 }
 
 body, input, button{
-    font-family:
+    font-family:'Montserrat', sans-serif;
+    font-size:18px;
+    border:none;
+    background:none;
+}
+
+html, body, #root{
+    max-width:100vw;
+    max-height:100vh;
+    height:100%;
+    width:100%;
+}
+
+#root{
+    --primary:#E63946; //red
+    --secondary:#F1FAEE; //white
+    --white:#fff;
+    --blue-menu:#457B9D;
+    --soft-blue:#A8DADC;
+
+
 }
 
 `
